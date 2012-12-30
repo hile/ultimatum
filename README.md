5c454c4f0a390e80d2e0dc3eaf437c59aaaf0d70
Ilkka Tuohela <hile@iki.fi>
2012-12-31 01:16:17 +0200

    Fixes against new systematic API

---
+++ README.md
@@ -0,0 +1,5 @@
+
+# Ultimatum BSD extensions to systematic #
+
+This module contains FreeBSD specific python extensions to systematic module. 
+
---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+"""
+Scripts and system tool wrappers for FreeBSD (and other *BSDs)
+
+This module is split from module to platform dependent tool
+"""
+
+import sys,os,glob
+from setuptools import setup
+
+VERSION='2.0.0'
+README = open(os.path.join(os.path.dirname(__file__),'README.txt'),'r').read()
+
+setup(
+    name = 'ultimatum',
+    keywords = 'System Management Utility FreeBSD Scripts',
+    description = 'Sysadmin utility modules and scripts for FreeBSD',
+    author = 'Ilkka Tuohela', 
+    author_email = 'hile@iki.fi',
+    long_description = README, 
+    version = VERSION,
+    url = 'http://tuohela.net/packages/ultimatum',
+    license = 'PSF',
+    zip_safe = False,
+    packages = ['ultimatum'],
+    scripts = glob.glob('bin/*'),
+    install_requires = [ 'systematic>=2.0.0' ],
+)
+
