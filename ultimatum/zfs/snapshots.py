fc052af4d4d2756b524b9e14103d36da5dc8c163
Ilkka Tuohela <hile@iki.fi>
2013-12-19 07:12:31 +0200

    Fix indent of tabs

--- ultimatum/zfs/snapshots.py
+++ ultimatum/zfs/snapshots.py
@@ -6,45 +6,43 @@ from ultimatum.zfs import execute, ZFSError, SNAPSHOT_DATE_FORMAT
 
 class ZFSSnapshot(list):
     def __init__(self, name):
-    	self.name = name
-    	try:
-        	self.volume, self.tag = name.split('@')
+        self.name = name
+        try:
+            self.volume, self.tag = name.split('@')
         except ValueError:
-        	raise ZFSError('Invalid snapshot name: %s' % name)
+            raise ZFSError('Invalid snapshot name: %s' % name)
 
     def __cmp__(self, other):
-    	if isinstance(other, basestring):
+        if isinstance(other, basestring):
             try:
                 tag = self.name.split('@')[1]
                 if tag == other:
                     return 0
-                else:
-                    print 'No match for %s and %s' % (tag, other)
             except IndexError:
                 pass
 
-    		return cmp(self.name, other)
+            return cmp(self.name, other)
 
-    	elif not isinstance(other,ZFSSnapshot):
-    		raise ZFSError("Can't compare ZFSSnapshot to %s object" % type(other))
+        elif not isinstance(other,ZFSSnapshot):
+            raise ZFSError("Can't compare ZFSSnapshot to %s object" % type(other))
 
-    	if self.volume != other.volume:
-    		return cmp(self.volume, other.volume)
+        if self.volume != other.volume:
+            return cmp(self.volume, other.volume)
 
-    	try:
-    		my_date = time.strptime(self.tag, SNAPSHOT_DATE_FORMAT)
-    		other_date = time.strptime(other.tag, SNAPSHOT_DATE_FORMAT)
-    		return cmp(my_date, other_date)
-    	except ValueError:
-    		pass
+        try:
+            my_date = time.strptime(self.tag, SNAPSHOT_DATE_FORMAT)
+            other_date = time.strptime(other.tag, SNAPSHOT_DATE_FORMAT)
+            return cmp(my_date, other_date)
+        except ValueError:
+            pass
 
-    	return cmp(self.tag, other.tag)
+        return cmp(self.tag, other.tag)
 
     def __eq__(self,other):
-    	return self.__cmp__(other) == 0
+        return self.__cmp__(other) == 0
 
     def __ne__(self,other):
-    	return self.__cmp__(other) != 0
+        return self.__cmp__(other) != 0
 
     def __repr__(self):
         return '%s@%s' % (self.volume, self.tag)
