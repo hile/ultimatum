0d6ba9f5c07bf290f31d6392182fb259af3c2b2f
Ilkka Tuohela <ilkka.tuohela@codento.com>
2015-03-04 09:30:01 +0200

    Point package URL to github

--- setup.py
+++ setup.py
@@ -5,13 +5,11 @@ Scripts and system tool wrappers for FreeBSD (and other *BSDs)
 This module is split from module to platform dependent tool
 """
 
-import sys
-import os
 import glob
 
 from setuptools import setup, find_packages
 
-VERSION='4.0.6'
+VERSION='4.0.7'
 
 setup(
     name = 'ultimatum',
@@ -20,14 +18,14 @@ setup(
     author = 'Ilkka Tuohela',
     author_email = 'hile@iki.fi',
     version = VERSION,
-    url = 'http://tuohela.net/packages/ultimatum',
+    url = 'https://github.com/hile/ultimatum',
     license = 'PSF',
     zip_safe = False,
     packages = find_packages(),
     scripts = glob.glob('bin/*'),
     install_requires = (
-        'seine>=2.4.1',
-        'systematic>=4.2.0',
+        'seine>=3.0.2',
+        'systematic>=4.2.6',
     ),
 )
 
